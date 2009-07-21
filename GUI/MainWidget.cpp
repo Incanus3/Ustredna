b1b09ad29165c4e4be791728f08a81e9443f9e54
Jakub Kaláb <jakubkalab@gmail.com>
2009-07-21 20:13:03 +0200

    zfunkcneni prvniho navrhu GUI

--- GUI/MainWidget.cpp
+++ GUI/MainWidget.cpp
@@ -5,8 +5,8 @@
 MainWidget::MainWidget(QWidget* parent) : QWidget(parent)
 {
 	lists = new QListWidget[6];
-	QHBoxLayout mainLayout;
+	QHBoxLayout* mainLayout = new QHBoxLayout;
 	for(int i = 0; i < 6; i++)
-		mainLayout.addWidget(&lists[i]);
-	setLayout(&mainLayout);
+		mainLayout->addWidget(&lists[i]);
+	setLayout(mainLayout);
 }
