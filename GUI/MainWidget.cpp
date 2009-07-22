3bdc2764275e35f14e9a5333c249259acd34906e
Jakub Kaláb <jakubkalab@gmail.com>
2009-07-22 12:54:27 +0200

    pridany GUI prvky pro zobrazovani datovych polozek

--- GUI/MainWidget.cpp
+++ GUI/MainWidget.cpp
@@ -1,12 +1,45 @@
 #include "MainWidget.h"
+#include <QtGui/QBoxLayout>
 
-#include <QtGui/QHBoxLayout>
+const short int MainWidget::listsNumber = 5;
 
 MainWidget::MainWidget(QWidget* parent) : QWidget(parent)
 {
-	lists = new QListWidget[6];
-	QHBoxLayout* mainLayout = new QHBoxLayout;
-	for(int i = 0; i < 6; i++)
-		mainLayout->addWidget(&lists[i]);
+	lists = new QListWidget[listsNumber];
+	QHBoxLayout* listsLayout = new QHBoxLayout;
+	for(int i = 0; i < listsNumber; i++)
+	{
+		lists[i].setMinimumWidth(160);
+		lists[i].setMinimumHeight(400);
+		listsLayout->addWidget(&lists[i]);
+	}
+
+	departmentLabel = new QLabel(tr("Oddělení:"));
+	nameLabel = new QLabel(tr("Jméno:"));
+	phonesLabel = new QLabel(tr("Linka:"));
+	cellsLabel = new QLabel(tr("Ručka:"));
+
+	departmentEdit = new QLineEdit;
+	nameEdit = new QLineEdit;
+	phone1Edit = new QLineEdit;
+	phone2Edit = new QLineEdit;
+	cell1Edit = new QLineEdit;
+	cell2Edit = new QLineEdit;
+
+	QHBoxLayout* dataLayout = new QHBoxLayout;
+	dataLayout->addWidget(departmentLabel);
+	dataLayout->addWidget(departmentEdit);
+	dataLayout->addWidget(nameLabel);
+	dataLayout->addWidget(nameEdit);
+	dataLayout->addWidget(phonesLabel);
+	dataLayout->addWidget(phone1Edit);
+	dataLayout->addWidget(phone2Edit);
+	dataLayout->addWidget(cellsLabel);
+	dataLayout->addWidget(cell1Edit);
+	dataLayout->addWidget(cell2Edit);
+
+	QVBoxLayout* mainLayout = new QVBoxLayout;
+	mainLayout->addLayout(listsLayout);
+	mainLayout->addLayout(dataLayout);
 	setLayout(mainLayout);
 }
--- GUI/MainWidget.h
+++ GUI/MainWidget.h
@@ -3,6 +3,8 @@
 
 #include <QtGui/QWidget>
 #include <QtGui/QListWidget>
+#include <QtGui/QLabel>
+#include <QtGui/QLineEdit>
 
 class MainWidget : public QWidget
 {
@@ -12,7 +14,22 @@ class MainWidget : public QWidget
 	MainWidget(QWidget* parent = 0);
 
 		private:
+	static const short int listsNumber;
+
 	QListWidget* lists;
+
+	QLabel* departmentLabel;
+	QLabel* nameLabel;
+	QLabel* phonesLabel;
+	QLabel* cellsLabel;
+
+	QLineEdit* departmentEdit;
+	QLineEdit* nameEdit;
+	QLineEdit* phone1Edit;
+	QLineEdit* phone2Edit;
+	QLineEdit* cell1Edit;
+	QLineEdit* cell2Edit;
+
 };
 
 #endif // MAINWIDGET_H
